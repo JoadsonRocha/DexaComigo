ea4e2e6670b659f777e69cebaf3d6b0319f1609e
Joadson Rocha <62960392+JoadsonRocha@users.noreply.github.com>
2026-08-07 17:19:07 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+import scrollbarHide from 'tailwind-scrollbar-hide';
+import animate from 'tailwindcss-animate';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './index.tsx',
+    './App.tsx',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './context/**/*.{js,ts,jsx,tsx}',
+    './services/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          50: '#fff1f2',
+          100: '#ffe4e6',
+          200: '#fecdd3',
+          300: '#fda4af',
+          400: '#fb7185',
+          500: '#f43f5e',
+          600: '#e11d48',
+          700: '#be123c',
+          800: '#9f1239',
+          900: '#881337',
+        },
+      },
+    },
+  },
+  plugins: [animate, scrollbarHide],
+};
