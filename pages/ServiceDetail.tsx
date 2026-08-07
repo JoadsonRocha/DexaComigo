10cce7e3048d4925c88c87af8ba9cae93703ce77
Joadson Rocha <62960392+JoadsonRocha@users.noreply.github.com>
2026-08-07 14:51:00 -0400

    Update ServiceDetail.tsx

--- pages/ServiceDetail.tsx
+++ pages/ServiceDetail.tsx
@@ -179,9 +179,19 @@ const ServiceDetail: React.FC = () => {
     setIsLocating(true);
     navigator.geolocation.getCurrentPosition(async (pos) => {
         try {
-            const res = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${pos.coords.latitude}&longitude=${pos.coords.longitude}&localityLanguage=pt`);
+            const res = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode?latitude=${pos.coords.latitude}&longitude=${pos.coords.longitude}&localityLanguage=pt`);
             const data = await res.json();
-            const loc = `${data.city || data.locality}, ${data.principalSubdivisionCode?.split('-')[1] || data.principalSubdivision}`;
+            const informative = data.localityInfo?.informative || [];
+            const road = informative.find((i: any) => ['road', 'street'].includes(i.description))?.name;
+            const neighborhood = informative.find((i: any) => ['neighbourhood', 'neighborhood', 'suburb'].includes(i.description))?.name;
+            const city = data.city || informative.find((i: any) => i.description === 'city')?.name || informative.find((i: any) => i.description === 'locality')?.name;
+            const state = data.principalSubdivisionCode?.split('-')[1] || data.principalSubdivision || informative.find((i: any) => i.description === 'principalsubdivision')?.name;
+            const parts: string[] = [];
+            if (road) parts.push(road);
+            if (neighborhood) parts.push(neighborhood);
+            if (city) parts.push(city);
+            if (state) parts.push(state);
+            const loc = parts.join(', ');
             setScheduleData(prev => ({ ...prev, location: loc }));
         } catch (e) {
             alert('Erro ao buscar localização.');
